aee3bccd2c193ce09d6e9f7b82a250ee5a8a6018
Taylor Seamans <taylor.seamans@yahoo.com>
2022-08-02 11:58:35 -0700

    Use Page components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,69 @@
 import React from 'react';
 import Link from 'next/link';
-import { Anchor, Box, grommet, Grommet, Heading, Paragraph } from 'grommet';
+import {
+  Anchor,
+  grommet,
+  Grommet,
+  Page,
+  PageContent,
+  PageHeader,
+  Paragraph,
+} from 'grommet';
 import { FormNext } from 'grommet-icons';
 
 const Index = () => (
   <Grommet theme={grommet} full>
-    <Box pad="large">
-      <Heading size="small">Hello!</Heading>
-      <Paragraph>
-        Hopefully this template helps jumpstart your project. This app is setup
-        to integrate Grommet with Next.js.
-      </Paragraph>
-      <Paragraph>
-        For help with grommet, check out the{' '}
-        <Anchor
-          href="https://v2.grommet.io/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          grommet documentation
-        </Anchor>
-        .
-      </Paragraph>
-      <Paragraph>
-        For help with Next.js, check out the{' '}
-        <Anchor
-          href="https://nextjs.org/docs/getting-started"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Next.js documentation
-        </Anchor>
-        .
-      </Paragraph>
-      <Paragraph>
-        Want to improve this starter? File an issue or contribute{' '}
-        <Anchor
-          href="https://github.com/taysea/grommet-nextjs-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          here
-        </Anchor>
-        !
-      </Paragraph>
-      <Box align="start">
+    <Page>
+      <PageContent>
+        <PageHeader
+          title="Hello!"
+          description={`Hopefully this template helps jumpstart your project. 
+        This app is setup to integrate Grommet with Next.js.`}
+        />
+        <Paragraph>
+          For help with grommet, check out the{' '}
+          <Anchor
+            href="https://v2.grommet.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            grommet documentation
+          </Anchor>
+          .
+        </Paragraph>
+        <Paragraph>
+          For help with Next.js, check out the{' '}
+          <Anchor
+            href="https://nextjs.org/docs/getting-started"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Next.js documentation
+          </Anchor>
+          .
+        </Paragraph>
+        <Paragraph>
+          Want to improve this starter?{' '}
+          <Anchor
+            href="https://github.com/taysea/grommet-nextjs-starter"
+            target="_blank"
+            rel="noopener noreferrer"
+            label="File an issue or contribute"
+          />
+          !
+        </Paragraph>
         {/* Need to pass href because of:
          * https://github.com/zeit/next.js/#forcing-the-link-to-expose-href-to-its-child */}
         <Link href="/secondpage" passHref>
-          <Anchor label="Second page" icon={<FormNext />} reverse />
+          <Anchor
+            alignSelf="start"
+            label="Second page"
+            icon={<FormNext />}
+            reverse
+          />
         </Link>
-      </Box>
-    </Box>
+      </PageContent>
+    </Page>
   </Grommet>
 );
 
--- src/pages/secondpage.js
+++ src/pages/secondpage.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import Link from 'next/link';
-import { Anchor, Box, grommet, Grommet, Heading, Paragraph } from 'grommet';
+import {
+  Anchor,
+  grommet,
+  Grommet,
+  Page,
+  PageContent,
+  PageHeader,
+} from 'grommet';
 import { FormNext } from 'grommet-icons';
 
 const SecondPage = () => (
   <Grommet theme={grommet} full>
-    <Box pad="large">
-      <Heading size="small">Second Page</Heading>
-      <Paragraph>
-        Next.js automatically handles the route based on your file structure.
-      </Paragraph>
-      <Box align="start">
+    <Page>
+      <PageContent>
+        <PageHeader
+          title="Second Page"
+          description={`Next.js automatically handles the route based 
+          on your file structure.`}
+        />
         {/* Need to pass href:
          * https://github.com/zeit/next.js/#forcing-the-link-to-expose-href-to-its-child */}
         <Link href="/" passHref>
-          <Anchor label="Back to Home page" icon={<FormNext />} reverse />
+          <Anchor
+            alignSelf="start"
+            label="Back to Home page"
+            icon={<FormNext />}
+            reverse
+          />
         </Link>
-      </Box>
-    </Box>
+      </PageContent>
+    </Page>
   </Grommet>
 );
 
